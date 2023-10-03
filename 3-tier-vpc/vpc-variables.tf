92ba6c9e9a8bf8617190747a59cb84f56602b451
Paolo Invernizzi <paolo.invernizzi@lightcast.com>
2023-10-03 17:54:32 +0200

    Add 3 tier VPC example

---
+++ 3-tier-vpc/vpc-variables.tf
@@ -0,0 +1,47 @@
+variable "vpc_name" {
+    default = "my-vpc"
+    description = "VPC name"
+    type = string
+}
+
+variable "vpc_cidr_block" {
+    default = "10.0.0.0/16"
+    description = "VPC CIDR block"
+    type = string
+}
+
+variable "vpc_azs" {
+    default = ["eu-west-1a", "eu-west-1b"]
+    description = "VPC availability zones"
+    type = list(string)
+}
+
+variable "vpc_public_subnets" {
+    default = ["10.0.101.0/24", "10.0.102.0/24"]
+    description = "VPC public subnets"
+    type = list(string)
+}
+
+variable "vpc_private_subnets" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+    description = "VPC private subnets"
+    type = list(string)
+}
+
+variable "vpc_database_subnets" {
+    default = ["10.0.151.0/24", "10.0.152.0/24"]
+    description = "VPC DBs subnets"
+    type = list(string)
+}
+
+variable vpc_enable_nat_gateway {
+    default = true
+    description = "Enable NAT gateway for the VPC"
+    type = bool
+}
+
+variable vpc_single_nat_gateway {
+    default = true
+    description = "Enable only NAT gateway for the VPC"
+    type = bool
+}
---
+++ 3-tier-vpc/vpc.tf
@@ -0,0 +1,40 @@
+module "vpc" {
+    source  = "terraform-aws-modules/vpc/aws"
+    version = "5.1.2"
+
+    name = "${local.name}-${local.vpc_name}"
+    cidr = var.vpc_cidr_block
+    azs             = var.vpc_azs
+    private_subnets = var.vpc_private_subnets
+    public_subnets  = var.vpc_public_subnets
+
+    create_database_subnet_group = true
+    create_database_subnet_route_table = true
+    database_subnets = var.vpc_database_subnets
+    # you'll need those to make your DB publicly available
+    # create_database_nat_gateway_route = true
+    # create_database_internet_gateway_route = true
+
+    # NAT gateway for outbound communication
+    enable_nat_gateway = var.vpc_enable_nat_gateway
+    single_nat_gateway = var.vpc_single_nat_gateway # likely false in production
+
+    # VPC DNS - true in most cases
+    enable_dns_hostnames =  true
+    enable_dns_support = true
+
+    public_subnet_tags = {
+        Name = "public-subnets"
+    }
+
+    private_subnet_tags = {
+        Name = "private-subnets"
+    }
+
+    database_subnet_tags = {
+        Name = "database-subnets"
+    }
+
+    tags = local.common_tags
+    vpc_tags = local.common_tags
+}
